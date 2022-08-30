6df70b346ad47f3fb562f4cd3118069976e8b572
Crowdbotics <team@crowdbotics.com>
2022-08-30 05:53:14 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/absolute_delivery_36954/MainActivity.java
@@ -0,0 +1,15 @@
+package com.absolute_delivery_36954;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "absolute_delivery_36954";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "absolute_delivery_36954",
+  url: "https://absolute_delivery_36954.botics.co",
+  api: "https://absolute_delivery_36954.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
